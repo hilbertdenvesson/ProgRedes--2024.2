d4af7e10f1c19c7d72c5d4f3b6c0d4823209194e
hilbertdenvesson <hilbert.santos@escolar.ifrn.edu.br>
2025-01-01 17:45:22 -0300

    Update udp-file-client-size.py

--- Unidade01-Avaliacao02/Q1/client/udp-file-client-size.py
+++ Unidade01-Avaliacao02/Q1/client/udp-file-client-size.py
@@ -5,42 +5,49 @@ SERVER = '127.0.0.1'
 PORT = 12345
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Evita travar no terminal
 sock.settimeout(10)
+
 
 while True:
     try:
-        fileName = input("Arquivo a pedir ao servidor: ").strip()  # Ignora espaços extras
+        fileName = input("Arquivo a pedir ao servidor: ").strip()
+        # Volta ao início do loop caso ele o usuário digite o nome vazio
         if fileName == "":
             print("Nome de arquivo vazio.")
-            
-        
-        # Envia o nome do arquivo ao servidor
-        print("Enviando pedido a", (SERVER, PORT), "para", fileName)
-        sock.sendto(fileName.encode('utf-8'), (SERVER, PORT))
+            continue
+
+        print("Enviando pedido ao servidor:", (SERVER, PORT), "para", fileName)
+        sock.sendto(fileName.encode('utf-8'), (SERVER, PORT))  
 
         # Recebe o tamanho do arquivo
         data, source = sock.recvfrom(4)
         file_size = int.from_bytes(data, "big")
-        
+
+        # Volta ao início do loop caso ele o usuário digite o nome vazio
         if file_size == 0:
-            print("Arquivo não encontrado ou vazio no servidor.")
-        else:
-            print(f"Tamanho do arquivo recebido: {file_size} bytes")
+            print(f"Arquivo '{fileName}' não encontrado ou está vazio no servidor.")
+            continue
 
+        print(f"Tamanho do arquivo recebido: {file_size} bytes")
+        print("Gravando arquivo localmente.")
+        fd = open(DIRBASE + fileName, 'wb')
         
-            print("Gravando arquivo localmente")
-            fd = open(DIRBASE + fileName, 'wb')
-       
-            file_received = 0
-            while file_received < file_size:
-                data, source = sock.recvfrom(4096)
-                fd.write(data)
-                file_received += len(data)
+        file_received = 0
+        while file_received < file_size:
+            data, source = sock.recvfrom(4096)
+            fd.write(data)  
+            file_received += len(data)
+            
+            # Envia ACK para o servidor confirmando o recebimento
+            sock.sendto(b'ACK', source) 
         
-            fd.close()
+        fd.close()
+        print(f"Arquivo '{fileName}' gravado em '{DIRBASE}{fileName}'")
 
-            print(f"Arquivo '{fileName}' gravado")
-     except Exception as erro:
-        print(f"Erro: {erro}")
+    except Exception as erro:
+        print(f"Erro no cliente: {erro}")
+        break
 
 sock.close()
