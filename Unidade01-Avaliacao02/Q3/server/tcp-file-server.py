b97a766c85cb3de7ea982db088c02afb1e14c9f2
hilbertdenvesson <hilbert.santos@escolar.ifrn.edu.br>
2025-01-02 12:34:08 -0300

    Update tcp-file-server.py

--- Unidade01-Avaliacao02/Q3/server/tcp-file-server.py
+++ Unidade01-Avaliacao02/Q3/server/tcp-file-server.py
@@ -1,1 +1,52 @@
+import socket
+import os
 
+DIRBASE = "files/"
+INTERFACE = '127.0.0.1'
+PORT = 12345
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind((INTERFACE, PORT))
+sock.listen(1)
+
+sock.settimeout(60)
+
+print("Servidor TCP escutando em ...", (INTERFACE, PORT))
+
+while True:
+    try:
+        conn, addr = sock.accept()
+        print("Conexão recebida de:", addr)
+
+        # Recebe o nome do arquivo 
+        fileName = conn.recv(512).decode('utf-8').strip()
+        print("Pedido para o arquivo:", fileName)
+
+        file_path = os.path.join(DIRBASE, fileName)
+        if os.path.exists(file_path):
+            file_size = os.path.getsize(file_path) if os.path.exists(file_path) 
+        else: 
+            file_size = 0
+        conn.sendall(file_size.to_bytes(4, 'big'))
+
+        if file_size == 0:
+            print(f"Arquivo '{fileName}' não encontrado.")
+            conn.close()
+            continue
+
+        print("Enviando arquivo:", fileName)
+        with open(file_path, 'rb') as fd:
+            while True:
+                fileData = fd.read(4096)
+                if fileData == "":
+                    break
+                conn.sendall(fileData)
+
+        print(f"Envio do arquivo '{fileName}' concluído.")
+        conn.close()
+
+    except Exception as erro:
+        print(f"Erro no servidor: {erro}")
+        break
+
+sock.close()
