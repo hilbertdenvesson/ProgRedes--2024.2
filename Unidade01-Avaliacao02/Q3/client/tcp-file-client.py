502985d32192b05d4f9ac71ef1e782814a0b13cf
hilbertdenvesson <hilbert.santos@escolar.ifrn.edu.br>
2025-01-03 16:50:28 -0300

    Update tcp-file-client.py

--- Unidade01-Avaliacao02/Q3/client/tcp-file-client.py
+++ Unidade01-Avaliacao02/Q3/client/tcp-file-client.py
@@ -18,30 +18,33 @@ while True:
             continue
 
         print("Enviando pedido ao servidor:", fileName)
-        sock.sendall(fileName.encode('utf-8'))
 
-        # Recebe o tamanho do arquivo
-        file_size_data = sock.recv(4)
-        file_size = int.from_bytes(file_size_data, "big")
+        # Envia o comprimento do nome do arquivo (2 bytes) e o nome do arquivo
+        fileNameBytes = fileName.encode('utf-8')
+        sock.sendall(len(fileNameBytes).to_bytes(2, 'big') + fileNameBytes)
 
-        if file_size == 0:
+        # Recebe o sinalizador e tamanho do arquivo
+        file_size_data = sock.recv(2)
+        file_size_flag = file_size_data
+        if file_size_flag == b'\x00\x01':
             print(f"Arquivo '{fileName}' não encontrado ou está vazio no servidor.")
             continue
+
+        # Recebe o tamanho do arquivo (4 bytes)
+        file_size_data = sock.recv(4)
+        file_size = int.from_bytes(file_size_data, "big")
 
         print(f"Tamanho do arquivo recebido: {file_size} bytes")
         print("Gravando arquivo localmente.")
         
+        # Recebe e grava o arquivo
         fd = open(os.path.join(DIRBASE, fileName), 'wb')
-        try:
-            file_received = 0
-            while file_received < file_size:
-                data = sock.recv(4096)
-                if data == "":
-                    break
-                fd.write(data)
-                file_received += len(data)
-        finally:
-            fd.close()
+        tam = file_size
+        while tam > 0:
+            data = sock.recv(4096)
+            fd.write(data)
+            tam -= len(data)
+        fd.close()
 
         print(f"Arquivo '{fileName}' gravado em '{DIRBASE}{fileName}'")
 
